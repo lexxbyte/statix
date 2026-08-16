0bc3597517fc307726263bf070db19d3d9b3f38d
Aleksa Milijanovic <aleksamilijanovic28@gmail.com>
2026-08-17 01:00:18 +0200

    Generate pages recursively from content dir (lesson 5.4)

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from pathlib import Path
 
 from extract_title import extract_title
 from markdown_to_html import markdown_to_html_node
@@ -35,11 +36,22 @@ def generate_page(from_path, template_path, dest_path):
         f.write(html)
 
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
+
+
 def main():
     if os.path.exists("public"):
         shutil.rmtree("public")
     copy_files_recursive("static", "public")
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content", "template.html", "public")
 
 
 if __name__ == "__main__":
