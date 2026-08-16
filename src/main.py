4e4460d84c2343799ea2d74fda268acaf436abe4
Aleksa Milijanovic <aleksamilijanovic28@gmail.com>
2026-08-17 00:54:32 +0200

    Add static assets and recursive copy to public (lesson 5.1)

--- src/main.py
+++ src/main.py
@@ -1,9 +1,25 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+
+def copy_files_recursive(source_dir_path, dest_dir_path):
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
+
+    for filename in os.listdir(source_dir_path):
+        from_path = os.path.join(source_dir_path, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        print(f" * {from_path} -> {dest_path}")
+        if os.path.isfile(from_path):
+            shutil.copy(from_path, dest_path)
+        else:
+            copy_files_recursive(from_path, dest_path)
 
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(node)
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+    copy_files_recursive("static", "public")
 
 
 if __name__ == "__main__":
